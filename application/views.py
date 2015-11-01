704fe51da7dc36f555cf01e4313d70f67c7900a3
rahul93anand <rahul93anand@gmail.com>
2015-11-02 00:33:01 +0530

    adding return email to customer

--- application/views.py
+++ application/views.py
@@ -10,9 +10,9 @@ def ContactUs(request):
         detail = request.POST
         name = detail.get("name")
         email = detail.get("email")
-        message = detail.get("message")
+        message = detail.get("message") + "    EMAIL ID :  " + email
         send_mail("Hey We got a message from " + name, message, 'foodsquare10@gmail.com', ['foodsquare10@gmail.com'])
-
+        send_mail("Thanks For sharing" , "Dear " +  name + " thanks for sharing valuable feedback with us, our team will get back to you shortly xD", "foodsquare10@gmail.com", [email])
         return render(request,"index.html",{'status' : "Message sent"})
 
 
